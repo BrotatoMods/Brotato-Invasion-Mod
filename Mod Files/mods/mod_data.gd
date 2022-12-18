a51ba9bfae8d137510b8d08b2ffea8d7154fb158
ithinkandicode <43499897+ithinkandicode@users.noreply.github.com>
2022-12-18 15:37:45 +0000

    update multi mod to 1.1.0

--- Mod Files/mods/mod_data.gd
+++ Mod Files/mods/mod_data.gd
@@ -6,3 +6,7 @@ export (Array, Resource) var weapons = []
 export (Array, Resource) var characters = []
 
 export (Array, Array, Resource) var weapons_characters = []
+
+# Array of items to always add. Can be used to add an item that provides info
+# on the mod (eg. the "! Invasion Mod" item in Invasion)
+export (Array, Resource) var debug_items = []
